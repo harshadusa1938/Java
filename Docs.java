655abedd12e8ae212fe6495562e282b88e597241
Harshad Patel <33075626+harshadusa1938@users.noreply.github.com>
2017-12-25 12:09:26 -0800

    Core_Java_Documentation

---
+++ Docs.java
@@ -0,0 +1,13 @@
+package com.thread;
+
+
+/*Thread
+handle by JVM
+Class thread and Interface Thread.
+Start--Runnable--Non-Runnable
+Start-Process-End
+Lock and unlock
+Shareable Variable
+
+Singletask Thread
+Multitask Thread*/
